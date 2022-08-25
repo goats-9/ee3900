35fa41460ca565c2f95eba746f9a2684af07ee12
goats-9 <goats.singh@gmail.com>
2022-08-25 10:45:16 +0530

    Update codes

--- filter/codes/6_3.py
+++ filter/codes/6_3.py
@@ -35,12 +35,12 @@ for k in range(0,N):
 
 #print(X)
 y = np.real(y)/N
+
 #plots
-plt.stem(range(0,N),y)
+plt.stem(range(0,N),y,markerfmt='C0o')
 plt.xlabel('$n$')
 plt.ylabel('$y(n)$')
 plt.grid()# minor
-
 #If using termux
 plt.savefig('../figs/6_3.png')
 #subprocess.run(shlex.split("termux-open ../figs/yndft.pdf"))
