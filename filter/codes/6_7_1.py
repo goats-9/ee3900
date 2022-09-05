80cac91c86cecd5ca5732ae16c1b5f35b39d4e5d
goats-9 <goats.singh@gmail.com>
2022-09-05 09:58:23 +0530

    Minor code edits

--- filter/codes/6_7_1.py
+++ filter/codes/6_7_1.py
@@ -4,22 +4,25 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 def nlgn(n, a): return a*n*np.log2(n)
-def nsq(n, a): return (a*n)**2
+def nsq(n, a): return a*(n**2)
+def lgn(n, a): return a*np.log2(n);
 
 a = 2**(np.arange(21))
 a1 = np.loadtxt('fft.txt', dtype='double')
 a2 = np.loadtxt('ifft.txt', dtype='double')
+plt.plot(a, a1, 'o')
+plt.plot(a, a2, 'o')
 b = np.linspace(1, 2**20 + 1, 2**20)
 popt, pcov = curve_fit(nlgn, a, a2)
 p1 = nlgn(b, *popt)
-p2 = nsq(b, *popt)
-p3 = np.log2(b)
-#plots
-plt.plot(a, a1, 'o')
-plt.plot(a, a2, 'o')
 plt.plot(b, p1)
+popt, pcov = curve_fit(nsq, a, a2)
+p2 = nsq(b, *popt)
 plt.plot(b, p2)
+popt, pcov = curve_fit(lgn, a, a2)
+p3 = lgn(b, *popt)
 plt.plot(b, p3)
+#plots
 plt.legend(["Simulation (FFT)", "Simulation (IFFT)", "Analysis (n$\log$n)", "Analysis (n$^2$)", "Analysis ($\log$n)"])
 plt.xlabel('n')
 plt.ylabel('T(n) (ms)')
