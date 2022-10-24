9c54ccf04e9c81ffd75de7b3c366529ac2c20b1b
Gautam Singh <goats.singh@gmail.com>
2022-10-24 21:13:03 +0530

    Finish assignment

--- charger/codes/4_3.py
+++ charger/codes/4_3.py
@@ -5,13 +5,14 @@ import os
 f = 50
 V = 5
 A = 12
-N = 100
-l = 5
-t = np.linspace(-l, l, N)
-s1 = np.sinc(t)
+N = 100000
+fc = 100
+tp = 100000
+t = np.linspace(-tp, tp, N)
+s1 = fc*np.sinc(fc*t)
 s2 = A*np.abs(np.sin(2*np.pi*f*t))
-sc = np.convolve(s1, s2, mode='full')
-plt.plot(np.linspace(-2*l, 2*l, 2*N - 1), V*sc/sc[N])
+sc = np.convolve(s1, s2, mode='same')
+plt.plot(t, sc*(np.pi*V/(2*A))*(2*tp/N))
 plt.grid()
 plt.xlabel('t (s)')
 plt.ylabel('V')
