7daf0acfa3aca46fa504d104b802522d48fe939b
Gautam Singh <goats.singh@gmail.com>
2022-10-31 08:53:50 +0530

    Minor edits to shell script

--- charger/run.sh
+++ charger/run.sh
@@ -11,27 +11,34 @@ if [[ !(`which svn` && `which ngspice` && `which latexmk` && `which python3`) ]]
 fi
 
 #Download the source codes
-echo "Downloading source codes"
-svn co https://github.com/goats-9/ee3900-assignments/trunk/charger > /dev/null 2>&1
+echo "Downloading source codes..."
+svn co https://github.com/goats-9/ee3900-assignments/trunk/charger
 echo "DONE!"
 
 #Run python codes
 echo ""
-echo "Running python codes"
+echo "Running python codes..."
 cd charger/codes
 for file in *.py; do
-    echo $file
+    echo "Running python script $file"
     python3 $file > /dev/null 2>&1
 done
 echo "DONE!"
 
 #Compile latex code
 echo ""
-echo "Compiling latex code"
+echo "Compiling latex code..."
 cd ..
 latexmk -pdf main.tex > /dev/null 2>&1
 echo "DONE!"
 
-#(OPTIONAL) View the pdf
-#termux-open main.pdf
-#sh gopen.sh main.pdf
+#View the pdf
+echo ""
+read -p "Would you like to see the PDF? [y/n] " see
+if [[ $see == 'y' || $see == 'Y' || $see == '' ]]; then
+    if [[ $TERMUX_VERSION ]]; then
+        termux-open main.pdf
+    else
+        sh gopen.sh main.pdf
+    fi
+fi
