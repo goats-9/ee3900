c6ab050a7faee755bb919f6d37f35013aee61b13
Gautam Singh <goats.singh@gmail.com>
2022-10-31 07:01:31 +0530

    Update shell script

--- charger/run.sh
+++ charger/run.sh
@@ -10,7 +10,27 @@ if [[ !(`which svn` && `which latexmk` && `which python3`) ]]; then
 fi
 
 #Download the source codes
-svn co https://github.com/goats-9/ee3900-assignments/trunk/charger
+echo "Downloading source codes"
+svn co https://github.com/goats-9/ee3900-assignments/trunk/charger > /dev/null 2>&1
+echo "DONE!"
 
 #Run python codes
+echo ""
 echo "Running python codes"
+cd charger/codes
+for file in *.py; do
+    echo $file
+    python3 $file > /dev/null 2>&1
+done
+echo "DONE!"
+
+#Compile latex code
+echo ""
+echo "Compiling latex code"
+cd ..
+latexmk -pdf main.tex > /dev/null 2>&1
+echo "DONE!"
+
+#(OPTIONAL) View the pdf
+#termux-open main.pdf
+#sh gopen.sh main.pdf
