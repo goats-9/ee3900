79129842725448070955690ab75c650cdbf993f8
Gautam Singh <goats.singh@gmail.com>
2022-10-31 05:50:21 +0530

    Finish charger assignment

--- charger/codes/5_4.py
+++ charger/codes/5_4.py
@@ -3,14 +3,15 @@ from matplotlib import pyplot as plt
 import os
 from scipy import signal
 
-A = np.loadtxt('resp_butter.txt')
-n = 5
+A = np.loadtxt('resp_cheby.txt')
+n = 4
 fc = 60
-b, a = signal.butter(n, fc, analog=True)
+rp = 0.5
+b, a = signal.cheby1(n, rp, fc, analog=True)
 f, h = signal.freqs(b, a)
 plt.plot(A[:,0], A[:,1], '.')
 plt.semilogx(f, 20*np.log10(np.abs(h)))
 plt.grid()
 plt.legend(['Simulation', 'Analysis'])
-plt.savefig('../figs/5_3.png')
-os.system('sh gopen.sh ../figs/5_3.png')
+plt.savefig('../figs/5_4.png')
+os.system('sh gopen.sh ../figs/5_4.png')
